56f202cfe502a67286f7ff99517146b71aa96ade
gioknx <giovanikx@gmail.comx>
2016-11-05 18:45:07 -0200

    Added codeclimate test reporterr

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -7,6 +7,9 @@ Coveralls.wear!
 require "simplecov"
 SimpleCov.start
 
+require "codeclimate-test-reporter"
+CodeClimate::TestReporter.start
+
 require 'loterias_caixa'
 require 'minitest/autorun'
 require "minitest/reporters"
